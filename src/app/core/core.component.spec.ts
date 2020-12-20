6f7d27cce0cdc0f0ed2d936275a28d97b55fcf71
xbrancher <developer@xbranch.io>
2020-12-20 13:23:18 +0100

    chore: upgrade to angular 11

--- src/app/core/core.component.spec.ts
+++ src/app/core/core.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CoreComponent } from './core.component';
 
@@ -6,7 +6,7 @@ describe('CoreComponent', () => {
   let component: CoreComponent;
   let fixture: ComponentFixture<CoreComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [CoreComponent]
     })
